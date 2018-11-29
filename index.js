999551fb244ec9562406cd427feb6d44d9acf1ac
Hafeez <feeziman007@hotmail.com>
2018-11-30 01:02:14 +1100

    update tests and killling process after test

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-//importing node framework
-var express = require('express');
-
+var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
-//Respond with "hello world" for requests that hit our root "/"
-app.get('/', function (req, res) {
-    res.send('Hello World!!!');
-});
-//listen to port 3000 by default
-app.listen(process.env.PORT || 3000);
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+var router = express.Router();
 
-module.exports = app;
+router.get('/', function (req, res) {
+    res.json({
+        "error": false,
+        "message": "Hello !",
+        "pid": process.pid
+    });
+});
+
+router.post('/add', function (req, res) {
+    res.json({
+        "error": false,
+        "message": "success",
+        "data": req.body.num1 + req.body.num2
+    });
+});
+
+router.get('/exit', function (req, res) {
+    res.end();
+    console.log('Killing process ', process.pid);
+    process.kill(process.pid);
+});
+
+
+app.use('/', router);
+
+app.listen(3000, function () {
+    console.log("I am listening at PORT 3000", process.pid);
+});
+
+process.on('SIGINT', function () {
+    console.log("\nGracefully shutting down from SIGINT (Ctrl-C)", process.pid);
+    // some other closing procedures go here
+    process.exit();
+});
--- test/test.js
+++ test/test.js
@@ -1,9 +1,68 @@
-var request = require('supertest');
-var app = require('../index');
-describe('GET / ', function () {
-    it('respond with hello world', function (done) {
-        //navigate to root and check the the response is "hello world"
-        request(app).get('/').expect('Hello World!!!');
-        done();
+var supertest = require("supertest");
+var should = require("should");
+var pid = 0;
+
+// This agent refers to PORT where program is runninng.
+
+var server = supertest.agent("http://localhost:3000");
+
+// UNIT test begin
+
+describe("SAMPLE unit test", function () {
+
+    // #1 should return home page
+    after(function () {
+        killProcess();
     });
+
+    it("should return home page", function (done) {
+
+        // calling home page api
+        server
+            .get("/")
+            .expect("Content-type", /json/)
+            .expect(200) // THis is HTTP response
+            .end(function (err, res) {
+                // HTTP status should be 200
+                res.status.should.equal(200);
+                // Error key should be false.
+                res.body.error.should.equal(false);
+                pid = res.body.pid;
+                done();
+            });
+    });
+
+    it("should add two number", function (done) {
+
+        //calling ADD api
+        server
+            .post('/add')
+            .send({
+                num1: 10,
+                num2: 20
+            })
+            .expect("Content-type", /json/)
+            .expect(200)
+            .end(function (err, res) {
+                res.status.should.equal(200);
+                res.body.error.should.equal(false);
+                res.body.data.should.equal(30);
+                done();
+            });
+    });
+
+    it("should return 404", function (done) {
+        server
+            .get("/random")
+            .expect(404)
+            .end(function (err, res) {
+                res.status.should.equal(404);
+                done();
+            });
+    });
+
+    function killProcess() {
+        console.log('PID::', pid);
+        process.kill(pid);
+    }
 });
