6aea2f45f81a746616b7a81293361360a3b0a867
Hafeez <feeziman007@hotmail.com>
2018-11-29 22:41:29 +1100

    callback done moved

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var app = require('../index');
 describe('GET / ', function () {
     it('respond with hello world', function (done) {
         //navigate to root and check the the response is "hello world"
-        request(app).get('/').expect('Hello World!!!', done);
-        // done();
+        request(app).get('/').expect('Hello World!!!');
+        done();
     });
 });
